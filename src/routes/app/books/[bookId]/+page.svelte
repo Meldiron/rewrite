06d31b0d94f1932bc7d02bc8b951b63ad284d07f
Matej Bačo <matejbaco2000@gmail.com>
2024-04-30 19:56:41 +0200

    Update +page.svelte

--- src/routes/app/books/[bookId]/+page.svelte
+++ src/routes/app/books/[bookId]/+page.svelte
@@ -105,20 +105,14 @@
 	</div>
 
 	{#if tableDocuments.length === 0}
-		<div role="alert" class="alert bg-base-100">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-				class="stroke-info shrink-0 w-6 h-6"
-				><path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="2"
-					d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-				></path></svg
-			>
-			<span>No pages found left.</span>
+		<div role="alert" class="alert text-success bg-base-100">
+			
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="shrink-0 w-5 h-5">
+				<path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+			  </svg>
+
+			  
+			<span>No pages left to rewrite.</span>
 		</div>
 	{:else}
 		<div class="card bg-base-100 shadow-xl p-3">
